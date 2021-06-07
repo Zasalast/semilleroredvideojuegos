68d3d1a480675dd9e56da83a9ad228c17f353c94
DesarrolloColectivo <zasalastobon@gmail.com>
2021-06-07 09:39:51 -0500

    add datos json

--- src/scenes/Team.jsx
+++ src/scenes/Team.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
- 
+import React from "react";
+import data from "./../Data.json";
+
+import { Card, Col, Row, Avatar } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+
+const { Meta } = Card;
 const Team = () => {
-    return (
-     
-        <h2>Team</h2>
-    )
-    
-}
-export default Team
+  const { userId, description, fistName, job } = data.team;
+  return (
+    <>
+      <h2>Team</h2>;
+      <div className="site-card-wrapper">
+        {" "}
+        <Row gutter={16}>
+          {" "}
+          <a href="https://sites.google.com/view/redes3d/qui%C3%A9nes-somos">
+            <Card
+              style={{ width: 300 }}
+              cover={<img alt="example" src={UserAddOutlined} />}
+            >
+              <Meta
+                avatar={
+                  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                }
+                title={fistName}
+                description={description}
+              />
+            </Card>
+          </a>
+          <Card
+            style={{ width: 300 }}
+            cover={<img alt="example" src={UserAddOutlined} />}
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title="¿Qué hacemos? "
+              description="This is the description"
+            />
+          </Card>
+          <Card
+            style={{ width: 300 }}
+            cover={<img alt="example" src={UserAddOutlined} />}
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title="Trabaja con Nosotros"
+              description="This is the description"
+            />
+          </Card>
+          ,
+        </Row>
+      </div>
+    </>
+  );
+};
+export default Team;
